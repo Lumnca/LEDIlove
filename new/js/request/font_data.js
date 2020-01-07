de4081ae1082c663a6b53f0232200c345fb16ae0
Key <34916202+Lumnca@users.noreply.github.com>
2020-01-07 22:13:06 +0800

    数据发送处理

--- new/js/request/font_data.js
+++ new/js/request/font_data.js
@@ -1,14 +1,14 @@
-function draw_font(data,dom,size,foreground_color) {
-	
+function draw_font(data, dom, size, foreground_color) {
+
 	var arraydata = data.join('');
-	
+
 	var canvas = document.createElement('canvas');
 	var ctx2 = canvas.getContext('2d');
 	//显示框的点阵方块大小默认96px
-	
+
 	canvas.width = 96;
 	canvas.height = 96;
-	
+
 	//size*size的点阵信息
 	for (let i = 0; i < size; i++) {
 		for (let j = 0; j < size; j++) {
@@ -22,3 +22,92 @@ function draw_font(data,dom,size,foreground_color) {
 	}
 	dom.appendChild(canvas);
 }
+
+
+function data_deal(src_data,font_size) {
+	var meta_data = [];
+	//数据处理方式由8位2进制转10进制
+	src_data.forEach((item, index) => {
+
+		let data = new Array();
+		
+		for (var i = 0; i < item.data.length; i++) {
+
+			let dec = parseInt(item.data[i], 2);
+
+			data.push(dec);
+
+		}
+		//应对硬件数据格式处理
+		if (font_size == 16) {
+
+			for (let i in data) {
+				if (i % 2 == 0) {
+					meta_data.push(data[i]);
+				}
+			}
+
+			for (let i in data) {
+				if (i % 2 == 1) {
+					meta_data.push(data[i]);
+				}
+			}
+		} else {
+			for (let i in data) {
+				if (i % 4 == 0) {
+					meta_data.push(data[i]);
+				}
+			}
+
+			for (let i in data) {
+				if (i % 4 == 1) {
+					meta_data.push(data[i]);
+				}
+			}
+			for (let i in data) {
+				if (i % 4 == 2) {
+					meta_data.push(data[i]);
+				}
+			}
+			for (let i in data) {
+				if (i % 4 == 3) {
+					meta_data.push(data[i]);
+				}
+			}
+		}
+
+	});
+
+
+
+
+	var sendmeta_data = [];
+	//将数据封装成16字节每段的数据
+	var meta = [];
+	if (font_size == 16) {
+		for (var i = 0; i < meta_data.length; i++) {
+			if (i % 32 == 0) {
+				meta_data[i] =999;// app.colors[i / 32]["value"];
+				//console.log("Color:" + meta_data[i] + " " + app.colors[i / 32]["rgba"]);
+			}
+			meta.push(meta_data[i]);
+			if ((i + 1) % 16 == 0) {
+				sendmeta_data.push(meta);
+				meta = [];
+			}
+		}
+	} else {
+		for (var i = 0; i < meta_data.length; i++) {
+			if (i % 128 == 0) {
+				meta_data[i] =  999 ;//app.colors[i / 128]["value"];
+			}
+			meta.push(meta_data[i]);
+			if ((i + 1) % 16 == 0) {
+				sendmeta_data.push(meta);
+				meta = [];
+			}
+		}
+	}
+	return sendmeta_data;
+}
+
